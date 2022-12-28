177893638cd9b9200066d0a2cba09908c85d0da5
Karim Madani <imkarimkarim@pm.me>
2022-12-28 15:35:09 +0330

    feat: add store name if not

---
+++ src/components/dashboard/SetStoreNameIfNot.tsx
@@ -0,0 +1,121 @@
+import { useMemo, useState } from 'react';
+import { handleError } from '../../modules/errorHandler';
+import { pb } from '../../modules/pocketbase';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useSnackbar } from 'notistack';
+import { randomName } from '../../modules/randomName';
+
+const SetStoreNameIfNot = () => {
+  const [show, setShow] = useState(false);
+  const [isSet, setIsSet] = useState(false);
+  const [storeName, setStoreName] = useState('');
+
+  useMemo(() => {
+    pb.collection('stores')
+      .getList(1, 1, {
+        filter: `owner = "${pb.authStore.model?.id}"`,
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.items.length > 0) {
+          localStorage.setItem('store', res.items[0].id);
+          setIsSet(true);
+        } else {
+          setShow(true);
+        }
+      })
+      .catch((err) => {
+        handleError(err, 'setStoreNameIfNot');
+      });
+  }, [isSet]);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const createStore = (storeName: string, callback: Function) => {
+    const data = {
+      owner: pb.authStore?.model?.id,
+      name: storeName,
+      username: pb.authStore?.model?.username,
+    };
+
+    pb.collection('stores')
+      .create(data)
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem('store', res.id);
+        setIsSet(true);
+        setShow(false);
+        callback && callback();
+      })
+      .catch((err) => {
+        handleError(err, 'SetStoreNameIfNot - handleSubmit', enqueueSnackbar);
+      });
+  };
+
+  const handleSubmit = () => {
+    createStore(storeName, () => {
+      enqueueSnackbar('Store Name saved.', { variant: 'success' });
+    });
+  };
+
+  const handleClose = () => {
+    createStore(randomName(), () => {
+      enqueueSnackbar('you can always changed it in settings', {
+        variant: 'info',
+      });
+      enqueueSnackbar('we picked a random store name for you', {
+        variant: 'success',
+      });
+    });
+  };
+
+  if (show)
+    return (
+      <Dialog open={show}>
+        <DialogTitle>
+          Welcome to coffeeMenu 👋
+          <br /> Lest start by naming Your Store!
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            what should we call your store? (this will be displayed to end
+            customers)
+          </DialogContentText>
+          <TextField
+            onKeyPress={(e) => e.key === 'Enter' && handleSubmit()}
+            margin="dense"
+            id="name"
+            label="Store Name"
+            type="name"
+            fullWidth
+            variant="standard"
+            required
+            autoFocus
+            value={storeName}
+            onChange={(e) => {
+              setStoreName(e.target.value);
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>i will choose later</Button>
+          <Button type="submit" variant="contained" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+
+  // modal, input,toasts, place holder, lets start by choosing you store name
+  // what we should call you store?
+
+  return <></>;
+};
+
+export default SetStoreNameIfNot;
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
 import DashboardLayout from '../../components/dashboard/DashboardLayout';
 import IntroAnimation from '../../components/dashboard/IntroAnimation';
+import SetStoreNameIfNot from '../../components/dashboard/SetStoreNameIfNot';
 
 const Dashboard = () => {
   return (
     <DashboardLayout>
+      <SetStoreNameIfNot />
       <IntroAnimation />
       test
     </DashboardLayout>
