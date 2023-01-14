f2e13ee9de1f1ec9306d80030b4fd0e181a4c6c4
Karim Madani <imkarimkarim@pm.me>
2023-01-14 12:27:15 +0330

    feat(ui): add optional text to FullScreenLoading

--- src/components/shared/FullScreenLoading.tsx
+++ src/components/shared/FullScreenLoading.tsx
@@ -1,12 +1,23 @@
 import { Backdrop, CircularProgress } from '@mui/material';
+import { FC } from 'react';
 
-const FullScreenLoading = ({ open = false }) => {
+type Props = {
+  open: boolean;
+  children?: JSX.Element[] | JSX.Element | string;
+};
+const FullScreenLoading: FC<Props> = ({ open = false, children }) => {
   return (
     <Backdrop
-      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        color: '#fff',
+        zIndex: (theme) => theme.zIndex.drawer + 10000,
+      }}
       open={open}
     >
       <CircularProgress color="inherit" />
+      {children && children}
     </Backdrop>
   );
 };
