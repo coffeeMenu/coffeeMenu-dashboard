f62083c29d1f56797a03775d515d2581fe206468
Karim Madani <imkarimkarim@pm.me>
2023-01-14 12:27:42 +0330

    feat: add image compressor

---
+++ src/modules/compressImage.ts
@@ -0,0 +1,31 @@
+import Compressor from 'compressorjs';
+
+export const compressImage = (file: File): Promise<File | Blob | undefined> => {
+  return new Promise<File | Blob | undefined>((resolve, reject) => {
+    if (!file) {
+      reject();
+    }
+
+    new Compressor(file, {
+      strict: true,
+      checkOrientation: true,
+      minWidth: 0,
+      maxWidth: 0,
+      width: 1500,
+      height: 1500,
+      resize: 'cover',
+      quality: 0.8,
+      mimeType: 'auto',
+      // TODO test a transparent png
+      convertTypes: 'image/png',
+      convertSize: 5000000,
+      success(result) {
+        resolve(result);
+        console.log('res');
+      },
+      error(err) {
+        console.error(err.message);
+      },
+    });
+  });
+};
