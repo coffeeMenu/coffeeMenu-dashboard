8e7c3d14f702e5a7376f1667af761942d322fb6a
imkarimkarim <imkarimkarim@pm.me>
2023-02-04 19:33:33 +0330

    chore: add logConfig

--- src/logConfig.ts
+++ src/logConfig.ts
@@ -1,2 +1,4 @@
-export const log_productDrawerReducer = false;
-export const log_productDrawerChange = false;
+export const log_productDrawerReducer = 1;
+export const log_addProductStateChange = 1;
+export const log_editProductStateChange = 1;
+export const log_validateForm = false;
